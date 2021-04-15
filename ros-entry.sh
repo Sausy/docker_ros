58721158497d5820e0d666f1f8abed405b979479
Sausy <Sausy@users.noreply.github.com>
2021-04-15 19:11:39 +0200

    Update ros-entry.sh
    bug fix, sourcing wrong ros version

--- ros-entry.sh
+++ ros-entry.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 #export ROS_MASTER_URI=http://localhost:11311
 export ROS_IP=$(hostname -I|head -n1 | awk '{print $1;'})
-source /opt/ros/melodic/setup.bash
+source /opt/ros/${ROSVERSION}/setup.bash
 
 roscore & sleep 10 && \
 roslaunch rosbridge_server rosbridge_websocket.launch & sleep 15 && \
